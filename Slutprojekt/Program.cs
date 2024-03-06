b682a5986c1fd882adcee5b5892f29573e9ace91
Isak Eriksson <isak.eriksson2@elev.ga.ntig.se>
2024-03-06 12:03:10 +0100

    Gjort listor

---
+++ Slutprojekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+List <string> länder = new(){"Tyskland", "Belgien", "Nederländerna", "Schweiz", "Albanien"};
+List <string> huvudstäder = new(){"Berlin", "Bryssel", "Amsterdam", "Bern", "Tirana"};
+List <string> valuta = new(){"Euro", "Euro", "Euro", "Franc", "Lek"};
+int i = Random.Shared.Next(4);
+Console.WriteLine(länder [i]);
+//string choice1 = Console.ReadLine().ToLower();
+
+Console.WriteLine($"Vad är huvudstaden i {länder[i]}?");
+Console.WriteLine($"{huvudstäder[i]}");
+Console.WriteLine($"Vilken valuta används i {länder[i]}");
+Console.WriteLine($"{valuta[i]}");
+
+Console.ReadLine(); 
