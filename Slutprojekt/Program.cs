ed857f1ccd2c4916ae4c594d5d3f18757766c511
Isak Eriksson <isak.eriksson2@elev.ga.ntig.se>
2024-04-10 10:56:26 +0200

    F

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,26 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class GeografiQuiz 
+
+
+List<string> länder = new() { "Tyskland", "Belgien", "Nederländerna", "Schweiz", "Albanien" };
+List<string> huvudstäder = new() { "Berlin", "Bryssel", "Amsterdam", "Bern", "Tirana" };
+List<string> valuta = new() { "Euro", "Euro", "Euro", "Franc", "Lek" };
+List<string> språk = new() {"Tyska","Flammländska", "Holländska", "Tyska", "Albanska"};
+
+Console.WriteLine("Välkommen till spelet! Vad heter du?");
+string namn = Console.ReadLine();
+
+if (namn == "noname")
 {
+    Console.WriteLine("Välkommen!");
+    Console.WriteLine("Du kommer att få ett slumpässigt land som du ska gissa vilket som är landets huvudstad, vilken valuta som används och landets befolkning");
 
-    static List <string> länder = new(){"Tyskland", "Belgien", "Nederländerna", "Schweiz", "Albanien"};
-    static List <string> huvudstäder = new(){"Berlin", "Bryssel", "Amsterdam", "Bern", "Tirana"};
-    static List <string> valuta = new(){"Euro", "Euro", "Euro", "Franc", "Lek"};
+}
+else
+{
+    Console.WriteLine("Välkommen, " + namn + "!");
+    Console.WriteLine("Du kommer att få ett slumpässigt land som du ska gissa vilket som är landets huvudstad, vilken valuta som används och landets befolkning");
+}
 
-    static void Main(string[] args)
+int i = Random.Shared.Next(4); //Slumpar ett land som ligger i listan
+int score = 0;
+static void KorrektSvar()
+{
+    Console.WriteLine("Korrekt!");
+    
+}
+
+Console.WriteLine($"Det slumpmässiga landet är {länder[i]}");
+Console.WriteLine($"Vad är huvudstaden i {länder[i]}?");
+for (int försök = 1; försök < 4; försök++)
+{
+    Console.WriteLine($"Vad är huvudstaden i {länder[i]}?");
+    string huvudstadGissning = Console.ReadLine();
+
+    if (huvudstadGissning.ToLower() == huvudstäder[i].ToLower())
     {
-        int i = Random.Shared.Next(4);
-
-        Console.WriteLine($"Det slumpmässiga landet är {länder [i]}");
-        
-        Console.WriteLine($"Vad är huvudstaden i {länder[i]}?");
-        Console.ReadLine();
-        Console.WriteLine($"{huvudstäder[i]}");
-        Console.WriteLine($"Vilken valuta används i {länder[i]}");
-        Console.ReadLine();
-        Console.WriteLine($"{valuta[i]}");
-
-        Console.ReadLine(); 
+        KorrektSvar();
+        score++;
+        break; // Bryt loopen om svaret är korrekt
+    }
+    else
+    {
+        Console.WriteLine($"Fel! Försök igen. Försök {försök}/3");
     }
 }
+Console.WriteLine($"Vilken valuta används i {länder[i]}");
+string valutaGissning = Console.ReadLine();
+if (valutaGissning.ToLower() == valuta[i].ToLower())
+{
+    KorrektSvar();
+    score++;
+}
+else
+{
+    while (valutaGissning != valuta[i])
+    {
+        Console.WriteLine("Fel!");
+        Console.ReadLine();
+        return;
+    }
+    
+}
+
+Console.WriteLine($"Vad är det mest talade språket i {länder[i]}");
+string språkGissning = Console.ReadLine();
+if (språkGissning.ToLower() == språk[i].ToLower())
+{
+    KorrektSvar();
+    score++;
+}
+else
+{
+    while (språkGissning != språk[i])
+    {
+        Console.WriteLine("Fel!");
+        Console.ReadLine();
+        return;
+    }
+    
+}
+
+
+
+
+//Console.WriteLine($"{valuta[i]}");
+
+
+Console.ReadLine();
+
+
+
+
--- Slutprojekt/obj/Debug/net6.0/Slutprojekt.AssemblyInfo.cs
+++ Slutprojekt/obj/Debug/net6.0/Slutprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49c746f37a2b69e89a271e8af1d3d473256c6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slutprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
